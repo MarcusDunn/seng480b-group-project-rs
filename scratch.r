4788d725fb95b148f5fa8384aa6c4d6e3b2e62af
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-11 21:15:11 -0800

    updated scratch

--- scratch.r
+++ scratch.r
@@ -1,31 +1,37 @@
+install.packages("ggplot2")
+install.packages("tidyverse")
+
 library(ggplot2)
 library(tidyverse)
 
-
 master = as_tibble(read.csv("/home/marcus/CLionProjects/seng480b-group-project-rs/master.csv", stringsAsFactors = TRUE))
-
 master$date = as.Date(as.POSIXct(master$seconds_since_epoch, origin="1970-01-01"))
-master$date
 
-master$date
+var_additons = filter(filter(master,  diff_type=="Addition"), declaration_type=="Var")
 
-ggplot(filter(filter(master,  diff_type=="Addition"), declaration_type=="Var"), aes(x=date, fill=project_name)) 
-  + scale_x_date(date_breaks = "12 month", date_labels =  "%Y") 
-  + geom_histogram(aes(y=cumsum(..count..)))
-
-ggplot(filter(filter(master,  diff_type=="Addition"), declaration_type=="Var"), aes(x=date, fill=project_name)) +
+ggplot(var_additons, aes(x=date, fill=project_name)) +
   scale_x_date(date_breaks = "12 month", date_labels =  "%Y") +
   facet_wrap(~project_name, ncol=1) +
   geom_histogram(bins=24) +
   scale_y_log10("Log Transformed var Declaration Additions") + 
   xlab("Year")
 
-ggplot(master, aes(x=indentation, fill=declaration_type)) +
-  geom_histogram(binwidth = 1) +
-  facet_wrap(~declaration_type, ncol=1, scale="free_y")
+bar_data = master %>%
+  filter(diff_type=="Addition") %>%
+  group_by(project_name) %>%
+  summarise(
+    var = sum(declaration_type=="Var"),
+    total = sum(diff_type=="Addition"), 
+    explict = sum(declaration_type!="Var"),
+    first_var_declaration=min(date) 
+  ) %>%
+  filter(var!=0)
+   
+bar_data
+ggplot(bar_data, aes(fill=project_name, x = reorder(project_name, var/total), y=var/total)) +
+  geom_bar(position="dodge", stat="identity") + 
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5)) +
+  ylab("Preportion of var Declarations") +
+  xlab("Project Name")
 
-count(master)
 
-mean(master$indentation)
-
-t.test(filter(master, declaration_type=="Type")$indentation, filter(master, declaration_type=="Var")$indentation, var.equal = FALSE)
